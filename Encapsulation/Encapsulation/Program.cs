df6166a36971b6b42697f956114c7e71c9ee4308
Akhmedzade <mrahhmedzade@gmail.com>
2025-10-29 11:25:21 +0400

    task id done

---
+++ Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,93 @@
+﻿using Encapsulation;
+using System;
+
+namespace UserApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User[] users = new User[3];
+
+            for (int i = 0; i < users.Length; i++)
+            {
+                Console.WriteLine($"\n{i + 1}. Istifadeci melumatlarini daxil edin:");
+
+                Console.Write("Fullname: ");
+                string fullname = Console.ReadLine();
+
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+
+                string password;
+                while (true)
+                {
+                    Console.Write("Password: ");
+                    password = Console.ReadLine();
+
+                    User tempUser = new User(email, password);
+                    if (tempUser.PasswordChecker(password))
+                    {
+                        tempUser.Fullname = fullname;
+                        users[i] = tempUser;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Password is not valid");
+                    }
+                }
+            }
+
+            int choice = -1;
+
+            while (choice != 0)
+            {
+                Console.WriteLine("\n--- MENYU ---");
+                Console.WriteLine("1. Show all users");
+                Console.WriteLine("2. Get info by id");
+                Console.WriteLine("0. Quit");
+                Console.Write("Seciminiz: ");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                if (choice == 1)  //Muellim burani switch case ile de yaza bilerik
+                {
+                    Console.WriteLine("\n--- All users ---");
+                    for (int i = 0; i < users.Length; i++)
+                    {
+                        Console.WriteLine(users[i].GetInfo());
+                    }
+                }
+                else if (choice == 2)
+                {
+                    Console.Write("Axtardiginiz ID-ni daxil edin: ");
+                    int id = Convert.ToInt32(Console.ReadLine());
+                    bool found = false;
+
+                    for (int i = 0; i < users.Length; i++)
+                    {
+                        if (users[i].Id == id)
+                        {
+                            Console.WriteLine(users[i].GetInfo());
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if (!found)
+                    {
+                        Console.WriteLine("Bu ID-li user tapılmadi.");
+                    }
+                }
+                else if (choice == 0)
+                {
+                    Console.WriteLine("Proqram sonlandirildi");
+                }
+                else
+                {
+                    Console.WriteLine("Yanlıs secim etdiniz!");
+                }
+            }
+        }
+    }
+}
---
+++ Encapsulation/Encapsulation/User.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace Encapsulation
+{
+    internal class User
+    {
+        private static int _idCounter = 0;
+        private int _id;
+        private string _fullname;
+        private string _email;
+        private string _password;
+        private byte _age;
+
+        public int Id
+        {
+            get { return _id; }
+        }
+
+        public string Fullname
+        {
+            get { return _fullname; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    _fullname = value;
+                }
+                else
+                {
+                    Console.WriteLine("Fullname bos ola bilmez!");
+                }
+            }
+        }
+
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value) && value.Contains("@") && value.Contains("."))
+                {
+                    _email = value;
+                }
+                else
+                {
+                    Console.WriteLine("Email duzgun formatda deyil!");
+                }
+            }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                if (PasswordChecker(value))
+                {
+                    _password = value;
+                }
+                else
+                {
+                    Console.WriteLine("Sifre teleblere cavab vermir!");
+                }
+            }
+        }
+
+        public byte Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+
+        public User(string email, string password)
+        {
+            _idCounter++;
+            _id = _idCounter;
+
+            _email = email;
+            _password = password;
+        }
+
+        public bool PasswordChecker(string password)
+        {
+            if (password.Length < 8) return false;
+
+            bool hasUpper = false;
+            bool hasLower = false;
+            bool hasDigit = false;
+
+            foreach (char c in password)
+            {
+                if (char.IsUpper(c)) hasUpper = true;
+                else if (char.IsLower(c)) hasLower = true;
+                else if (char.IsDigit(c)) hasDigit = true;
+            }
+
+            return hasUpper && hasLower && hasDigit;
+        }
+
+        public string GetInfo()
+        {
+            return $"ID: {Id}, Fullname: {Fullname}, Email: {Email}";
+        }
+    }
+}
