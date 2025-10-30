4e0d2113edb30086af0d08bad28767450d27c163
Akhmedzade <mrahhmedzade@gmail.com>
2025-10-31 00:53:15 +0400

    Task is done

---
+++ StringBuilderTask/StringBuilderTask/Helper.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBuilderTask
+{
+    public static class Helper
+    {
+        public static bool IsOdd(this int number)
+        {
+            return number % 2 != 0;
+        }
+
+        public static bool IsEven(this int number)
+        {
+            return number % 2 == 0;
+        }
+
+        public static bool HasDigit(this string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+                return false;
+
+            foreach (char c in text)
+            {
+                if (char.IsDigit(c))
+                    return true;
+            }
+            return false;
+        }
+
+        public static bool CheckPassword(this string password)
+        {
+            if (string.IsNullOrEmpty(password) || password.Length < 8)
+                return false;
+
+            bool hasUpper = false;
+            bool hasLower = false;
+            bool hasDigit = false;
+
+            foreach (char c in password)
+            {
+                if (char.IsUpper(c)) hasUpper = true;
+                else if (char.IsLower(c)) hasLower = true;
+                else if (char.IsDigit(c)) hasDigit = true;
+
+                if (hasUpper && hasLower && hasDigit)
+                    return true;
+            }
+
+            return hasUpper && hasLower && hasDigit;
+        }
+
+        public static string Capitalize(this string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return text;
+
+            if (text.Length == 1)
+                return text.ToUpper();
+
+            return char.ToUpper(text[0]) + text.Substring(1).ToLower();
+        }
+
+        public static string CustomSubstring(this string text, int startIndex)
+        {
+            if (text == null)
+                return "";
+
+            string result = "";
+            for (int i = startIndex; i < text.Length; i++)
+            {
+                result += text[i];
+            }
+            return result;
+        }
+        public static string CustomSubstring(this string text, int startIndex, int length)
+        {
+            if (text == null)
+                return "";
+
+            string result = "";
+            for (int i = startIndex; i < startIndex + length && i < text.Length; i++)
+            {
+                result += text[i];
+            }
+            return result;
+        }
+        public static bool CustomContains(this string text, string value)
+        {
+            if (text == null || value == null)
+                return false;
+
+            if (value == "")
+                return true;
+
+            for (int i = 0; i <= text.Length - value.Length; i++)
+            {
+                int j = 0;
+                while (j < value.Length && text[i + j] == value[j])
+                {
+                    j++;
+                }
+
+                if (j == value.Length)
+                    return true;
+            }
+
+            return false;
+        }
+
+    }
+}
---
+++ StringBuilderTask/StringBuilderTask/Program.cs
@@ -0,0 +1,19 @@
+﻿using StringBuilderTask;
+int x = 7;
+Console.WriteLine($"{x} tekdirmi? => {x.IsOdd()}");
+Console.WriteLine($"{x} cutdurnu? => {x.IsEven()}");
+
+string word = "Hello123";
+word.HasDigit();
+
+string password = "Code123A";
+Console.WriteLine($"Sufre duzgundumu? => {password.CheckPassword()}");
+
+string name = "emRaH";
+name = name.Capitalize();
+
+
+string text = "HelloWorld";
+Console.WriteLine($"CustomSubstring(5): {text.CustomSubstring(5)}");
+Console.WriteLine($"CustomSubstring(0, 5): {text.CustomSubstring(0, 5)}");
+Console.WriteLine($"CustomContains('loWo'): {text.CustomContains("loWo")}");
